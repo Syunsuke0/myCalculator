ef33da7476e1f4ddb1fce24e098d1679648671cd
syunsuke <s.syun617@icloud.com>
2026-07-09 23:11:15 +0900

    ✏️ タイポ修正

--- src/components/Keypad.jsx
+++ src/components/Keypad.jsx
@@ -1,12 +1,12 @@
 import { BUTTONS } from "@/constants/constants";
 
-export const Keypad = ({ OnKeyPressed }) => {
+export const Keypad = ({ onKeyPressed }) => {
   return (
     <div className=" grid  grid-cols-4">
       {BUTTONS.map((item) => (
         <button
           onClick={() => {
-            OnKeyPressed(item);
+            onKeyPressed(item);
           }}
           key={item}
           className=" h-12 bg-white text-black outline-1 font-bold text-3xl"
